2e05651b880af299f9674080e782f188938481e6
Paweł Sroka <srokap@gmail.com>
2023-03-01 02:31:49 +0100

    Backported changes lost in previous branch merges

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-var packagejson = require('./packages/open-lens/package.json');
+var packagejson = require('./lens/packages/open-lens/package.json');
 
 // name seems to serve as identifier
 packagejson.name = `open-lens-srokap`;
@@ -14,3 +14,10 @@ packagejson.build.publish = [{
 }];
 
 packagejson.build.win.artifactName = "OpenLens-Srokap.Setup.${version}.${ext}";
+
+if (process.platform != "win32") {
+    // build both x86_64 and arm64 for Linux and Darwin
+    packagejson.scripts['build:app'] = "electron-builder --publish onTag --x64 --arm64";
+}
+
+fs.writeFileSync('./lens/packages/open-lens/package.json', JSON.stringify(packagejson));
